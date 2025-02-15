284b04f0d451b76034a4ca11dd54fb6cd0b7d239
Hero Solutions <info@herosolutions.be>
2025-02-15 12:37:59 +0100

    Fixes routes in indexcontroller

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,40 +2,39 @@
 
 namespace App\Controller;
 
-use App\Document\Provider;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
-#[Route('/{_locale}', requirements: ['_locale' => '%app.locales%'])]
 class IndexController extends AbstractController
 {
-    private array $locales;
-    private TranslatorInterface $translator;
     private DocumentManager $documentManager;
+    private TranslatorInterface $translator;
+    private string $locales;
 
-    public function __construct(ParameterBagInterface $params, TranslatorInterface $translator, DocumentManager $documentManager)
+    public function __construct(DocumentManager $documentManager, TranslatorInterface $translator, string $locales)
     {
-        $this->locales = explode('|', $params->get('app.locales'));
-        $this->translator = $translator;
         $this->documentManager = $documentManager;
+        $this->translator = $translator;
+        $this->locales = $locales;
     }
 
     private function getBasicData(string $currentPage, Request $request): array
     {
-        $translatedRoutes = array_map(function ($locale) use ($currentPage, $request) {
-            return [
+        $translatedRoutes = [];
+
+        foreach (explode('|', $this->locales) as $locale) {
+            $translatedRoutes[] = [
                 'locale' => $locale,
                 'route' => $this->generateUrl($currentPage, ['_locale' => $locale]),
                 'active' => $locale === $request->getLocale(),
             ];
-        }, $this->locales);
+        }
 
-        $providers = $this->documentManager->getRepository(Provider::class)->findAll();
+        $providers = $this->documentManager->getRepository(\App\Document\Provider::class)->findAll();
 
         return [
             'current_page' => $currentPage,
@@ -45,49 +44,50 @@ class IndexController extends AbstractController
         ];
     }
 
-    #[Route('/', name: 'home')]
+    #[Route('/', name: 'home_default')]
+    #[Route('/{_locale}', name: 'home', requirements: ['_locale' => '%app.locales%'])]
     public function homeWithLocale(Request $request): Response
     {
         return $this->render("home.{$request->getLocale()}.html.twig", $this->getBasicData('home', $request));
     }
 
-    #[Route('/manual', name: 'manual')]
+    #[Route('/{_locale}/manual', name: 'manual', requirements: ['_locale' => '%app.locales%'])]
     public function manual(Request $request): Response
     {
         return $this->render("manual.{$request->getLocale()}.html.twig", $this->getBasicData('manual', $request));
     }
 
-    #[Route('/open_data', name: 'open_data')]
+    #[Route('/{_locale}/open_data', name: 'open_data', requirements: ['_locale' => '%app.locales%'])]
     public function openData(Request $request): Response
     {
         return $this->render("open_data.{$request->getLocale()}.html.twig", $this->getBasicData('open_data', $request));
     }
 
-    #[Route('/open_source', name: 'open_source')]
+    #[Route('/{_locale}/open_source', name: 'open_source', requirements: ['_locale' => '%app.locales%'])]
     public function openSource(Request $request): Response
     {
         return $this->render("open_source.{$request->getLocale()}.html.twig", $this->getBasicData('open_source', $request));
     }
 
-    #[Route('/legal', name: 'legal')]
+    #[Route('/{_locale}/legal', name: 'legal', requirements: ['_locale' => '%app.locales%'])]
     public function legal(Request $request): Response
     {
         return $this->render("legal.{$request->getLocale()}.html.twig", $this->getBasicData('legal', $request));
     }
 
-    #[Route('/500', name: '500')]
+    #[Route('/{_locale}/500', name: '500', requirements: ['_locale' => '%app.locales%'])]
     public function error500(Request $request): Response
     {
         return $this->render("error.html.twig", $this->getBasicData('500', $request));
     }
 
-    #[Route('/403', name: '403')]
+    #[Route('/{_locale}/403', name: '403', requirements: ['_locale' => '%app.locales%'])]
     public function error403(Request $request): Response
     {
         return $this->render("error403.html.twig", $this->getBasicData('403', $request));
     }
 
-    #[Route('/404', name: '404')]
+    #[Route('/{_locale}/404', name: '404', requirements: ['_locale' => '%app.locales%'])]
     public function error404(Request $request): Response
     {
         return $this->render("error404.html.twig", $this->getBasicData('404', $request));
